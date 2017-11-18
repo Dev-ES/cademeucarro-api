bade574152b0dfe5eef8259511543fc35976b25f
David Rock <davidrocha@outlook.com>
2017-11-18 15:18:03 -0200

    Swagger OK

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,15 @@ namespace cademeucarro_api
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "CadeMeuCarro",
+                    Version = "v1",
+                    Description = "MVP criado no Hackaton organizado pelo grupo de desenvolvedores do Dev-ES",
+                    TermsOfService = "None",
+                     Contact = new Contact { Name = "Dev-ES", Email = "", Url = "http://dev-es.org/" },
+                    // License = new License { Name = "Use under LICX", Url = "https://example.com/license" }
+                });
             });
         }
 
