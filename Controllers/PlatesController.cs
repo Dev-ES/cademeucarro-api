a06ce1bb6aa7c121406d031e66c74ef1400fa35a
Rafael dos Santos <p.4ever@gmail.com>
2017-11-18 15:06:06 -0200

    Added Sinesp Integration

--- Controllers/PlatesController.cs
+++ Controllers/PlatesController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using cademeucarro_api.Models;
+using cademeucarro_api.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace cademeucarro_api.Controllers
 {
@@ -21,21 +24,30 @@ namespace cademeucarro_api.Controllers
         [HttpGet, Route("search")]
         public async Task<ActionResult> SearchPlate([FromQuery]string plate = "")
         {
+            var sinespSearch = await SinespSearch(plate);
+            
             var searchResult = await _context.Cars
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.Plate == plate);
 
-            if (searchResult == null)
+            if (searchResult != null)
             {
-                return Ok(new { stolen = false });
+                sinespSearch.IsStolen = searchResult.IsStolen;
+                sinespSearch.StolenOn = searchResult.StolenOn;
             }
 
-            return Ok(
-                new
-                {
-                    stolen = searchResult.IsStolen,
-                    stolenOn = searchResult.StolenOn
-                });
+            return Ok(sinespSearch);
+        }
+
+        private async Task<SearchResult> SinespSearch(string plate) 
+        {
+            const string sinespUrl = "http://18.231.13.29:3000/api/plate";
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetStringAsync($"{sinespUrl}/{plate}");
+                var data = JsonConvert.DeserializeObject<SearchResult>(result);
+                return data;
+            }
         }
 
         [HttpPost, Route("add")]
---
+++ ViewModels/SearchResult.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace cademeucarro_api.ViewModels
+{
+    public class SearchResult
+    {
+        public bool? IsStolen { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Status { get; set; }
+        public string Plate { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public DateTime? StolenOn { get; set; }
+    }
+}
